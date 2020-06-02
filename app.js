a84aeb17e82bec75c05990a08964aa5d7a0597a0
Filip Sjöberg <filip.sjoberg@hotmail.com>
2020-06-02 13:18:50 +0200

    changing routs

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(require('../routes'));
+app.use(require('./routes'));
 
 module.exports = app;
